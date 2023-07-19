7136c5480917ed30286eb7a2b5777f609286a2ad
Monika Sokolowska <monika@sokolowscy.net>
2023-07-19 17:09:18 +0200

    Update 04_01

--- src/main/java/HairSalon.java
+++ src/main/java/HairSalon.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 public class HairSalon {
 
     private String[] openingDays = {"Monday", "Tuesday"};
+    private int haircutPrice = 30;
+    private Calculator calculator = new Calculator();
 
     // This method should return the openingDays array.
     public String[] getOpeningDays() {
@@ -13,14 +15,22 @@ public class HairSalon {
     // If not it should return false. If the String passed in does not end in the letters "day",
     // it should throw an IllegalArgumentException with a message explaining that only weekdays are valid.
     public boolean isOpen(String weekDay) {
-        if(!weekDay.endsWith("day")) {
+        if (!weekDay.endsWith("day")) {
             throw new IllegalArgumentException("The weekday argument must end in the letters 'day'");
         }
-        if(Arrays.asList(openingDays).contains(weekDay)) {
+        if (Arrays.asList(openingDays).contains(weekDay)) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    public int getHaircutPrice() {
+        return haircutPrice;
+    }
+
+    public int applyDiscount() {
+        return calculator.divide(haircutPrice, 2) + 5;
+    }
+
 }
