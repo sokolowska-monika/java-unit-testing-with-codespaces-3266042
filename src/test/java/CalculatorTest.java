246a40586afc27b5e4a694514ec45f0c784fcebf
Monika Sokolowska <monika@sokolowscy.net>
2023-07-19 15:31:05 +0200

    Update 02_03

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,11 +8,37 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        int expected = 3;
 
+        int expected = 3;
         int actual = calculator.add(1, 2);
 
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testSubtract() {
+
+        int expected = 3;
+        int actual = calculator.subtract(5, 2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMultiply() {
+
+        int expected = 10;
+        int actual = calculator.multiply(5, 2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivide() {
+
+        int expected = 5;
+        int actual = calculator.divide(10, 2);
+
+        assertEquals(expected, actual);
+    }
 }
